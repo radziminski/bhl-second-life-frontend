a2a9a237482bdf1d30a6f34f45ebbba4f3e7a71f
Jan R <radziminski.j@gmail.com>
2020-11-07 01:06:27 +0100

    Added chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:SecondLife/widgets/Chat.dart';
 import 'package:SecondLife/widgets/DefaultAppBar.dart';
 import 'package:SecondLife/widgets/DefaultBottomNavBar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -18,24 +19,42 @@ class ChatScreen extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.2),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30),
-                  ),
-                ),
-                child: Center(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintStyle: TextStyle(
-                          color: Colors.white.withOpacity(0.6),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 2, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
                         ),
-                        hintText: 'Enter a search term'),
+                      ),
+                      child: Center(
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintStyle: TextStyle(
+                                color: Colors.white.withOpacity(0.6),
+                              ),
+                              hintText: 'Enter a message'),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 16.0, right: 8.0),
+                      child: IconButton(
+                        icon: Icon(
+                          CupertinoIcons.arrow_right_circle_fill,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                        onPressed: () {},
+                      )),
+                ],
               ),
             ),
             SizedBox(
